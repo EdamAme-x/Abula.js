00d6e50ab02991024da60bb39e7e2e47287140e0
EdamAme-x <edame8080@gmail.com>
2024-01-10 13:32:14 +0900

    add

---
+++ Abula.js/index.ts
@@ -0,0 +1,45 @@
+import { Hono } from 'hono';
+
+export type Abula = Hono & {
+    abula: boolean,
+    gets: (path: string[], ...handler: any[]) => Abula,
+    posts: (path: string[], ...handler: any[]) => Abula,
+    puts: (path: string[], ...handler: any[]) => Abula,
+    deletes: (path: string[], ...handler: any[]) => Abula,
+};
+
+export function createAbula(base: Hono): Abula {
+    // @ts-ignore
+    const app: Abula = base;
+    app.abula = true;
+    app.gets = (path: string[], ...handler: any[]) => {
+        for (const p in path) {
+            app.get(p, ...handler)
+        }
+
+        return app;
+    }
+    app.posts = (path: string[], ...handler: any[]) => {
+        for (const p in path) {
+            app.post(p, ...handler)
+        }
+
+        return app;
+    }
+    app.puts = (path: string[], ...handler: any[]) => {
+        for (const p in path) {
+            app.put(p, ...handler)
+        }
+
+        return app;
+    }
+    app.deletes = (path: string[], ...handler: any[]) => {
+        for (const p in path) {
+            app.delete(p, ...handler)
+        }
+
+        return app;
+    }
+
+    return app;
+}
---
+++ example/src/index.ts
@@ -0,0 +1,21 @@
+import { serve } from '@hono/node-server'
+import { Context, Hono } from 'hono'
+import { createAbula } from "../../Abula.js/index"
+
+const app = createAbula(new Hono());
+
+app.get('/', (c: Context) => {
+  return c.text('Hello Hono!')
+})
+
+app.gets(['/en', '/ja'], (c: Context) => {
+  return c.text((1 + 2).toString())
+})
+
+const port = 3000
+console.log(`Server is running on port ${port}`)
+
+serve({
+  fetch: app.fetch,
+  port
+})
